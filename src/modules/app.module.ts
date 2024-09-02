45ff69ce1c197f9e916614b9f4c975bc268aa36c
vientorepublic <markgod0419@gmail.com>
2024-09-02 15:37:56 +0900

    ✅ add winston http logger

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { MainModule } from './main.module';
 import { UploadModule } from './upload.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeormConfig } from 'src/database/database.provider';
 import { DownloadModule } from './download.service';
 import { TasksModule } from './tasks.module';
+import { LoggerMiddleware } from 'src/middlewares/logger.middleware';
 
 @Module({
   imports: [
@@ -17,4 +18,8 @@ import { TasksModule } from './tasks.module';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
